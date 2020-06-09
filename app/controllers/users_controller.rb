80a17feeb38a43a9dc23a05e73afbfc6815d4b6d
Amanda <main@Amandas-MacBook-Air.local>
2020-06-09 17:24:33 -0400

    adds show method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,8 @@ class UsersController < ApplicationController
   end
 
   def show 
+    @user = User.find_by_id(params[:id]) # nil if doesnt find anything ...find gives an error
+    redirect_to '/' if !@user
   end 
 
   private 
