a36ba1cadcaff5e7a6ea362f157e80bcaf6b6a18
Amanda <main@Amandas-MacBook-Air.local>
2020-06-09 17:31:07 -0400

    adds root, signup, logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+  root "sessions#home"
+
   get '/signup' => 'users#new'
+  post '/signup' => 'users#create' 
+  # these are to hide /users from the path
   
+  #login 
+  get '/login' => 'sessions#new'
+  post '/logout' => 'sessions#create'
+  
+  #logout 
+  delete '/logout' => 'sessions#destroy'
+
   
   resources :users
   resources :languages
