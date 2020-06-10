8c900bd840f2510594c5dad49fb69b62d5346174
Amanda <main@Amandas-MacBook-Air.local>
2020-06-09 21:52:24 -0400

    adds flash error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,7 +19,11 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to user_path(user) # '/users/#{user.id}'
     else  
+      flash[:message] = "Invalid Login Info, Please Try Again"
       redirect_to '/login'
     end
+  
   end
+
+
 end
