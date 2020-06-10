bd6ac3194958b471a590276074b62780903b3c81
Amanda <main@Amandas-MacBook-Air.local>
2020-06-10 17:20:00 -0400

    adds create vocab

--- app/controllers/vocabs_controller.rb
+++ app/controllers/vocabs_controller.rb
@@ -1,8 +1,25 @@
 class VocabsController < ApplicationController
 
+  def index 
+    @vocabs = current_user.vocabs.all
+  end 
+
   def new 
     @vocab = Vocab.new
   end 
   
+  def create 
+    @vocab = current_user.vocabs.build(vocab_params)
+    if @vocab.save
+      redirect_to vocabs_path 
+    else   
+      render :new
+    end
+  end
 
+  private 
+
+  def vocab_params 
+    params.require(:vocab).permit(:word_or_phrase, :memo)
+  end
 end
