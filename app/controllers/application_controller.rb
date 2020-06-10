358d5ca13719c4819c814e7b2fd18cab0886aa3e
Amanda <main@Amandas-MacBook-Air.local>
2020-06-10 00:24:52 -0400

    adds helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+# gives access  to methods in/from views
+  helper_method :current_user, :logged_in?
+
+  private 
 
   def current_user
     @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
@@ -11,5 +15,5 @@ class ApplicationController < ActionController::Base
   def redirect_if_not_logged_in
     redirect_to '/' if !logged_in?
   end
-  
+
 end
