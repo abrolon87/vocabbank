87aee64570c2c5094c23ec6204b2e2379f518033
Amanda <main@Amandas-MacBook-Air.local>
2020-06-14 00:57:33 -0400

    adds validations to vocab.rb

--- app/models/vocab.rb
+++ app/models/vocab.rb
@@ -2,4 +2,15 @@ class Vocab < ApplicationRecord
   belongs_to :user
   belongs_to :language
   has_many :examples
+  
+  validates :word_or_phrase, presence: true 
+  validates :translation, presence: true
+
+  accepts_nested_attributes_for :language
+
+#  def language_name=(name)
+#    self.language = Language.find_or_create_by(name: name)
+#  end
+
+ 
 end
