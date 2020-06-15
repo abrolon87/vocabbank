338713d098e4b1964a3b563ca8968bd0953dc5ac
Amanda <main@Amandas-MacBook-Air.local>
2020-06-14 20:34:25 -0400

    adds validations to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   has_secure_password
   has_many :vocabs
   has_many :languages, through: :vocabs
+
+  validates  :username, :email, presence: true, uniqueness: true
+  validates :name, :password_digest, presence: true
 end
 
 
