234a223eb13fadadfc1ebf057a7a5274e26db702
Amanda <main@Amandas-MacBook-Air.local>
2020-06-16 01:30:55 -0400

    removes email from validations for OAuth to work

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,13 @@ class User < ApplicationRecord
   has_many :vocabs
   has_many :languages, through: :vocabs
 
-  validates  :username, :email, presence: true, uniqueness: true
+  validates  :username, presence: true, uniqueness: true #email
   validates :name, :password_digest, presence: true
 
   def self.from_omniauth(auth)
     find_or_create_by(username: auth.info.nickname) do |user|
       user.username = auth.info.nickname
+      user.name = auth.info.name
       user.id = auth.uid
       user.password = SecureRandom.hex
     end
