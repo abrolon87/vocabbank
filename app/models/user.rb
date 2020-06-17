2a20a51127a64fade326fb13130e1fc0837f2906
Amanda <main@Amandas-MacBook-Air.local>
2020-06-17 12:37:31 -0400

    adds dependent

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,15 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :vocabs
+  has_many :vocabs, dependent: :delete_all
   has_many :languages, through: :vocabs
-
-  validates  :username, presence: true, uniqueness: true #email
+  
+  validates  :username, presence: true, uniqueness: true
   validates :name, :password_digest, presence: true
 
   def self.from_omniauth(auth)
     find_or_create_by(username: auth.info.nickname) do |user|
       user.username = auth.info.nickname
-      user.email = auth.info.email
+      #user.email = auth.info.email
       user.name = auth.info.name
       user.id = auth.uid
       user.password = SecureRandom.hex
