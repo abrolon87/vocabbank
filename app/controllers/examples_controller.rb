6a4ceace87bd779a5b5eab22bea9cd8f2aade58e
Amanda <main@Amandas-MacBook-Air.local>
2020-06-17 12:28:26 -0400

    app/controllers/examples_controller.rb

--- app/controllers/examples_controller.rb
+++ app/controllers/examples_controller.rb
@@ -1,2 +1,38 @@
 class ExamplesController < ApplicationController
+
+  def new
+    @example = Example.new
+  end
+  
+
+  def create
+    @example.vocab_id = Vocab.find(params[:id])
+    @example = Example.create(example_params)
+    
+    binding.pry
+    
+    if @example.save
+      
+      binding.pry
+    end
+  end
+   
+    
+
+  def show 
+    @example = Example.find_by(id: params[:id])
+  end
+
+
+  private 
+
+  def example_params 
+    params.require(:example).permit(:sentence, :vocab_id)
+  end
+
 end
+
+
+
+
+
