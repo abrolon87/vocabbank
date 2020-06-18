1ce7708f17ec72c3f3b40d3a037f119de05a1ccd
Amanda <main@Amandas-MacBook-Air.local>
2020-06-18 17:34:36 -0400

    adds scope method to vocab.rb

--- app/models/vocab.rb
+++ app/models/vocab.rb
@@ -3,10 +3,12 @@ class Vocab < ApplicationRecord
   belongs_to :language
   has_many :examples, dependent: :delete_all
   
-  validates :word_or_phrase, presence: true 
+  validates :word_or_phrase, presence: true, uniqueness: true 
   validates :translation, presence: true
 
   accepts_nested_attributes_for :language, reject_if: :all_blank
+
+  scope :sort_alphabetically, -> {order(word_or_phrase: :asc)}
 
   # def language_attributes=(attr_hash)
   #   self.build_language(attr_hash)
