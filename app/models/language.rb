f37b5840bc31f8e23eb6c8d10eb805d8015b8a7c
Amanda <main@Amandas-MacBook-Air.local>
2020-06-18 17:33:45 -0400

    adds validations to language.rb

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,4 +1,8 @@
 class Language < ApplicationRecord
   has_many :vocabs, dependent: :delete_all
   has_many :users, through: :vocabs #, source: :user
+
+  validates :language_name, presence: true, uniqueness: true
+
+  #scope :sort_alphabetically, -> {order(language_name: :asc)}
 end
