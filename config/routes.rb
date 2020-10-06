4bac0b19099bef1f9b6c573793247e885096d2fa
Amanda Rolon <abrolon87@gmail.com>
2020-10-06 10:31:20 -0400

    testing OAuth

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
   root "sessions#home"
 
   get '/signup' => 'users#new'
   post '/signup' => 'users#create' 
-  # these are to hide /users from the path
   
-  #login 
+  
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   
-  #logout 
+
   delete '/logout' => 'sessions#destroy'
   
-  match '/auth/github/callback', to: 'sessions#github_create', via: [:get, :post]
+  match 'auth/github/callback', to: 'sessions#github_create', via: [:get, :post]
 
   resources :users
   resources :languages
