d1f2569dd737313ddd43bb2f521898baf18b5520
Amanda Rolon <abrolon87@gmail.com>
2021-01-14 19:24:51 -0500

    adds google secrets

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,10 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+  #provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
+  {
+    scope: 'userinfo.email, userinfo.profile, http://gdata.youtube.com',
+    prompt: 'select_account',
+    image_aspect_ratio: 'square',
+    image_size: 50
+  }
 end
