80eb24fb4cc33120771210daa3e5a53037c76cf5
Amanda Rolon <abrolon87@gmail.com>
2021-03-18 15:18:59 -0400

    resets to a previous commit

--- Gemfile
+++ Gemfile
@@ -24,9 +24,10 @@ gem 'bcrypt', '~> 3.1.7'
 
 gem 'omniauth'
 gem 'omniauth-github'
+gem 'omniauth-google'
 gem 'dotenv-rails'
-
-
+gem 'omniauth-rails_csrf_protection', '~> 0.1'
+gem 'omniauth-google-oauth2'
 # gem 'devise'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,21 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
   root "sessions#home"
 
   get '/signup' => 'users#new'
   post '/signup' => 'users#create' 
-  # these are to hide /users from the path
   
-  #login 
+  
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   
-  #logout 
+
   delete '/logout' => 'sessions#destroy'
   
-  match 'http://vocabbank.herokuapp.com/auth/github/callback', to: 'sessions#github_create', via: [:get, :post]
-
+  match 'auth/github/callback' => 'sessions#github_create', via: [:get, :post]
+  #get 'auth/github/callback' => 'sessions#github_create'
+  
+  match 'auth/google_oauth2/callback' => 'sessions#github_create', via: [:get, :post] 
   resources :users
   resources :languages
   resources :vocabs
